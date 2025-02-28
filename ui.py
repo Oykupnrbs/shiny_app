86e94bff9f05c690eefe3283ba63c4198fc33772
Oykupnrbs <oykupinarbasi1@gmail.com>
2025-02-28 21:17:16 +0300

    Yeni değişiklikler eklendi

--- ui.py
+++ ui.py
@@ -1,6 +1,6 @@
 from shiny import ui
 
-def app_ui():
+def app_ui(request):
     return ui.page_fluid(
         ui.h2("Interactive Single-Cell UMAP Viewer"),
         ui.input_file("file_upload", "Upload ZIP File", multiple=False, accept=[".zip"]),
